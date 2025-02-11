a601542e70705d4aa78376ff651c102ea4b252d6
Ayobami Oyetola <72043879+Acekicker277@users.noreply.github.com>
2025-02-11 06:56:25 +0100

    Update Number_guessing_game.cpp

--- Number_guessing_game.cpp
+++ Number_guessing_game.cpp
@@ -1,1 +1,39 @@
+#include <stdio.h>
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+int main()
+{
+int guess;
+int tries;
+int number;
+srand(time(NULL));
+number = rand() % 100;
 
+do{
+cout << "******Guesssing Game********" << endl;
+
+cout << "Guess a number \n";
+cin >> guess;
+tries++;
+
+if (guess < number) 
+{
+    cout <<"too low! try again. \n";
+}
+
+else if (guess > number)
+{
+    cout << "too high! try again. \n";
+}
+
+else
+{
+cout << "CORRECT You are super duper! you made " <<  tries << " number(s) of tries \n";
+}
+}while (guess != number);
+
+cout << "******************";
+    return 0;
+}
